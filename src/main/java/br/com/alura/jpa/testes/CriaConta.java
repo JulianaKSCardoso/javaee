93f3a5078f113f8d8e898203b33854f011c348a0
Juliana <jkscardoso@apoioprodesp.sp.gov.br>
2021-03-04 17:58:18 -0300

    Aula 2 - Usando Entity Manager

--- src/main/java/br/com/alura/jpa/testes/CriaConta.java
+++ src/main/java/br/com/alura/jpa/testes/CriaConta.java
@@ -10,7 +10,15 @@ public class CriaConta {
     public static void main (String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
         EntityManager em = emf.createEntityManager();
+
+        Conta conta = new Conta();
+        conta.setTitular("Eduardo");
+        conta.setNumero(1234);
+        conta.setAgencia(4321);
+
+        em.getTransaction().begin();
+        em.persist(conta);
+        em.getTransaction().commit();
+        emf.close();
     }
-    Conta eduardo = new Conta();
-    
 }
